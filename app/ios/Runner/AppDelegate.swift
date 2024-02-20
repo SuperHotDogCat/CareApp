eaaf43d5e23dacf5ad8c4b43a520d0fb70a234f6
SuperHotDogCat <siromisochan@gmail.com>
2024-02-20 14:04:28 +0900

    Update .gitignore

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Flutter
-import Firebase // <- 追加
-import FirebaseCore // <- 追加
+import Firebase // <- Add
+import FirebaseCore // <- Add
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,7 +9,7 @@ import FirebaseCore // <- 追加
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()  // <- 追加
+    FirebaseApp.configure()  // <- Add
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
