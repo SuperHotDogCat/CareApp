74882210c3a42ed536a37ffb7fe9a23614afa860
kei <lab.sprout@gmail.com>
2024-02-21 15:13:30 +0900

    Webでの通知用（とりあえず通知をうけとるだけ）

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'pages.dart';
 import 'config.dart';
 //ios build, firebase iosを追加して手順に従う->https://qiita.com/kasa_le/items/fed9f25b92091bd162ce & https://zenn.dev/popy1017/articles/b9f3e46b5efeb79af1f7
@@ -13,6 +14,31 @@ Future<void> init() async {
           appId: configrations.appId,
           messagingSenderId: configrations.messagingSenderId,
           projectId: configrations.projectId));
+
+  // FCM用の設定 通知権限のリクエスト
+  final messagingInstance = FirebaseMessaging.instance;
+  NotificationSettings settings = await messagingInstance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  final fcmToken = await messagingInstance.getToken();
+
+  // トークンの取得
+  debugPrint('fcmToken: $fcmToken');
+
+  // TODO: メッセージの受信
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    debugPrint('Got a message whilst in the foreground!');
+    if (message.notification != null) {
+      debugPrint('${message.notification!.title}');
+      debugPrint('${message.notification!.body}');
+    }
+  });
 }
 
 void main() {
