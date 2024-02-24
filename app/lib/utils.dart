0ead8d816fcdbadced10f583c41acbc3f627646a
SuperHotDogCat <siromisochan@gmail.com>
2024-02-24 21:01:49 +0900

    Remove unused functions

--- app/lib/utils.dart
+++ app/lib/utils.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:csv/csv.dart';
@@ -14,36 +13,9 @@ void createUserBasicData(User user, Map userBasicProfile) {
   users.doc(user.uid).set(initialUserData);
 }
 
-Widget RealTimeStreamDataWidget(stream, builder) {
-  return StreamBuilder<QuerySnapshot>(stream: stream, builder: builder);
-}
-
-Widget RealTimeDataWidget(stream, builder) {
-  return StreamBuilder(stream: stream, builder: builder);
-}
-
 Stream<DocumentSnapshot> fetchUserDataSnapShots(User user) {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   return firestore.collection('users').doc(user.uid).snapshots();
-}
-
-Stream<DocumentSnapshot> fetchUserDataTest() {
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
-  return firestore
-      .collection('users')
-      .doc("azuxHrjNPFhkGkInWA0tQJXgpWG3")
-      .snapshots();
-}
-
-Map<String, dynamic> fetchUserDataWithNameAndId(User user) {
-  var userStream = fetchUserDataSnapShots(user);
-  Map<String, dynamic> userData = {};
-  userStream.listen((DocumentSnapshot snapshot) {
-    if (snapshot.exists) {
-      userData = snapshot.data() as Map<String, dynamic>;
-    }
-  });
-  return userData;
 }
 
 Future<List<List<dynamic>>> loadCsvData(String path) async {
